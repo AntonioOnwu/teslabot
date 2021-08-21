d3c18a3991e207c2161baedb2dae0d2b31e79cc7
AntonioOnwu <43736225+AntonioOnwu@users.noreply.github.com>
2021-08-20 22:07:16 -0500

    fixed video

--- src/BackgroundVid.js
+++ src/BackgroundVid.js
@@ -1,10 +1,10 @@
 import React from 'react'
 // import classes from './BackgroundVideo.module.css'
-import video from './FinalVid.mov'
+// import video from './FinalVid.mov'
 
 const BackgroundVid = () => {
     // const videoSource = video
-    const videoSource = 'https://www.w3schools.com/tags/movie.mp4'
+    const videoSource = 'https://teslabot-video.s3.us-east-2.amazonaws.com/FinalVid.mov'
     return (
         <div className ="App">
         <video autoPlay = "autoplay" loop ="loop" muted
